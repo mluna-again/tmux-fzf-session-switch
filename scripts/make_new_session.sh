350b4ce32da6abe9c0ecc15ca8459c4c47180dc8
Thuan Pham <phamtanthuan2311@gmail.com>
2021-09-14 11:41:55 +0700

    🌟feature(new session): disable confirm

--- scripts/make_new_session.sh
+++ scripts/make_new_session.sh
@@ -1,17 +1,12 @@
 #!/usr/bin/env bash
 
 SESSION_NAME=$1
-ANSWER=$2
 
 function main {
-  local session
-  local answer
-  session=$SESSION_NAME
-  answer=$ANSWER
+    local session
+    session="$SESSION_NAME"
 
-  if [ $answer == "y" ] || [ $answer == "Y" ]; then
     tmux new-session -d -s $session
     tmux switch-client -t $session
-  fi
 }
 main
