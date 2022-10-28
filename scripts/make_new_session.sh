e03b99dc225345ad1a6d1ca0e60122823e3293fc
Thuan Pham <thuanOwa@gmail.com>
2022-10-28 22:24:37 +0700

    clean code, try to support version 3.3a

--- scripts/make_new_session.sh
+++ scripts/make_new_session.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
 
 SESSION_NAME=$1
-ANSWER=$2
 
 function main {
-  local session
-  local answer
-  session=$SESSION_NAME
-  answer=$ANSWER
-
-  tmux new-session -d -s "$session"
-  tmux switch-client -t "$session"
+  tmux new-session -d -s "$SESSION_NAME"
+  tmux switch-client -t "$SESSION_NAME"
 }
 main
